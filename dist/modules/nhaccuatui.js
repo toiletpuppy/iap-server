f8eb64017d00e2aed7d1d1fcd4054628fac99830
Pham Duy <phd051199@gmail.com>
2023-09-02 21:23:01 +0700

    fix nct

--- dist/modules/nhaccuatui.js
+++ dist/modules/nhaccuatui.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    pattern: /^https:\/\/graph.nhaccuatui.com\/.*\/user\/account\/info/,
+    pattern: /https:\/\/graph.nhaccuatui.com\/.*?\/user(s)?\/(account\/)?info/,
     rewrite: (body) => {
         body.data.vipExpire = "05.11.2099";
         body.data.isVIP = true;
--- src/modules/nhaccuatui.ts
+++ src/modules/nhaccuatui.ts
@@ -1,7 +1,7 @@
 import { Body } from "../types";
 
 export default {
-  pattern: /^https:\/\/graph.nhaccuatui.com\/.*\/user\/account\/info/,
+  pattern: /https:\/\/graph.nhaccuatui.com\/.*?\/user(s)?\/(account\/)?info/,
   rewrite: (body: Body) => {
     body.data.vipExpire = "05.11.2099";
     body.data.isVIP = true;
