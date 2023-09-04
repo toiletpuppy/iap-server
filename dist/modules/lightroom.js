f07a6bb74c9a07cc6e3533d3849e1d53ac715a66
Pham Duy <phd051199@gmail.com>
2023-09-04 10:15:55 +0700

    test345

--- dist/modules/lightroom.js
+++ dist/modules/lightroom.js
@@ -3,38 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     pattern: /^https:\/\/photos.adobe.io\/v\d\/accounts/,
     rewrite: (body) => {
-        if (typeof body !== 'object') {
-            body = body.toString().replace(/while.{7}\n/, "");
-            body = JSON.parse(body);
-        }
-        body.entitlement.status = "subscriber";
-        body.current_subs = {
-            product_id: "lightroom",
-            store: "adobe",
-            purchase_date: "2019-10-10T16:32:10.254954Z",
-            sao: {
-                inpkg_CCES: "0",
-                inpkg_CCLE: "1",
-                inpkg_CCSN: "0",
-                inpkg_CCSV: "0",
-                inpkg_LCCC: "0",
-                inpkg_LPES: "0",
-                inpkg_LRBRL: "0",
-                inpkg_LRMAC: "0",
-                inpkg_LRMC: "0",
-                inpkg_LRMP: "0",
-                inpkg_LRTB: "0",
-                inpkg_PHLT: "0",
-                inpkg_PHLT2: "0",
-                inpkg_PLES: "0",
-                storage_quota: "100",
+        body = body.toString().replace(/while.{7}\n/, "");
+        body = JSON.parse(body);
+        body.entitlement = {
+            ...body.entitlement,
+            status: "subscriber",
+            current_subs: {
+                product_id: "lightroom",
+                store: "adobe",
+                purchase_date: "2019-10-10T16:32:10.254954Z",
+                sao: {
+                    inpkg_CCES: "0",
+                    inpkg_CCLE: "1",
+                    inpkg_CCSN: "0",
+                    inpkg_CCSV: "0",
+                    inpkg_LCCC: "0",
+                    inpkg_LPES: "0",
+                    inpkg_LRBRL: "0",
+                    inpkg_LRMAC: "0",
+                    inpkg_LRMC: "0",
+                    inpkg_LRMP: "0",
+                    inpkg_LRTB: "0",
+                    inpkg_PHLT: "0",
+                    inpkg_PHLT2: "0",
+                    inpkg_PLES: "0",
+                    storage_quota: "100",
+                },
+                storage: {
+                    used: 0,
+                    limit: 1154487209165,
+                    display_limit: 1099511627776,
+                    warn: 992137445376,
+                },
             },
-        };
-        body.entitlement.storage = {
-            used: 0,
-            limit: 1154487209165,
-            display_limit: 1099511627776,
-            warn: 992137445376,
         };
         body.avatar.placeholder = true;
         return body;
